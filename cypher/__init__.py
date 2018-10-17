a9f46d0a605eedaf579f7d10e11f814694aa84af
nicusor.picatureanu <nicolaepicatureanu@gmail.com>
2018-10-17 23:15:59 +0300

    added caesar cipher algorithm to encryt and decryt

---
+++ cypher/__init__.py
@@ -0,0 +1,9 @@
+try:
+    from cypher.caesarCipher import CaesarCipher
+    from cypher.caesarCipher import CaesarCipherError
+except ImportError:
+    from cypher.caesarCipher import CaesarCipher
+    from cypher.caesarCipher import CaesarCipherError
+
+__title__ = 'caesarcipher'
+__author__ = 'Nicusor Picaturanu'
